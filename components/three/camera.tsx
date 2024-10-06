350a1311caf405307e86fc2ab3f754a5386f3723
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-10-07 01:18:50 +0300

    test

--- components/three/camera.tsx
+++ components/three/camera.tsx
@@ -1,36 +1,36 @@
 'use client';
 
 import React, { useRef } from 'react';
-import { OrbitControls } from '@react-three/drei';
+import { MapControls } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
-import { OrbitControls as OrbitControlsType } from 'three-stdlib';
+import { MapControls as MapControlsType } from 'three-stdlib';
 
 export const Camera: React.FC = () => {
-    const controlsRef = useRef<OrbitControlsType | null>(null); // Specify the correct type here
+    const controlsRef = useRef<MapControlsType | null>(null);
+    // const maxZoom = 5;
+    // const maxX = 2;
 
     const minPan = new THREE.Vector3(-2, 1, -2);
     const maxPan = new THREE.Vector3(2, 5, 2);
-
     useFrame(() => {
         if (controlsRef.current) {
             const target = controlsRef.current.target;
 
-            // Restrict on the X axis
+            // Обмежуємо по осі X
             target.x = Math.max(minPan.x, Math.min(maxPan.x, target.x));
 
-            // Restrict on the Y axis
+            // Обмежуємо по осі Y
             target.y = Math.max(minPan.y, Math.min(maxPan.y, target.y));
 
-            // You can also restrict the camera position
+            // Можемо також додати обмеження для камери, якщо це потрібно
             const position = controlsRef.current.object.position;
             position.x = Math.max(minPan.x, Math.min(maxPan.x, position.x));
             position.y = Math.max(minPan.y, Math.min(maxPan.y, position.y));
         }
     });
-
     return (
-        <OrbitControls
+        <MapControls
             ref={controlsRef}
             enableZoom={true}
             enableRotate={false}
